4f5c0344a19d5fc9dcc1ccb9464186734232e5db
Adamgraph <telegraphon101@gmail.com>
2021-02-25 08:35:55 +0100

    all files up

---
+++ js/javascript1.js
@@ -0,0 +1,9 @@
+// open full screen search box
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+// close full screen search box
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
